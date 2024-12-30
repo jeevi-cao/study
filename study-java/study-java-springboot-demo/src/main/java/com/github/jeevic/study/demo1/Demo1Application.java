bf4bcda0f256e419153502f12f1ef8ed37c22ab6
jeevic <qq542561541@gmail.com>
2024-12-30 20:58:22 +0800

    Add: java study code

---
+++ study-java/study-java-springboot-demo/src/main/java/com/github/jeevic/study/demo1/Demo1Application.java
@@ -0,0 +1,18 @@
+package com.github.jeevic.study.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jeevi
+ */
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+}
+
+
+
